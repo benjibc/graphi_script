56a2226b740e79c4586deb3a2443b3a065d1817b
Benny Chen <youfychenbc5000@gmail.com>
2013-08-17 14:41:38 -0400

    provided config

--- connector.py
+++ connector.py
@@ -1,10 +1,11 @@
 import MySQLdb, MySQLdb.cursors
+import config
 
 class Connector:
     def __init__(self):
-        self.db=MySQLdb.connect(user="root", passwd="qwe110170",
-            db="wikidb", cursorclass=MySQLdb.cursors.SSCursor)
+        self.db=MySQLdb.connect(user=config.user, passwd=config.password,
+            db=config.db, cursorclass=MySQLdb.cursors.SSCursor)
         self.cursor = self.db.cursor()
-        self.db2=MySQLdb.connect(user="root", passwd="qwe110170",
-            db="wikidb")
+        self.db2=MySQLdb.connect(user=config.user, passwd=config.password,
+            db=config.db)
         self.cursor2 = self.db2.cursor()
